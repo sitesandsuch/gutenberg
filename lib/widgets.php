61f7fd739a89785145208d70cf61774aea326454
Jorge <jorge.costa@developer.pt>
2019-04-16 21:50:35 +0100

    Add WordPress Area CPT

--- lib/widgets.php
+++ lib/widgets.php
@@ -126,3 +126,58 @@ function gutenberg_legacy_widget_settings( $settings ) {
 	return $settings;
 }
 add_filter( 'block_editor_settings', 'gutenberg_legacy_widget_settings' );
+
+
+function create_wp_area_post_type() {
+	register_post_type(
+		'wp_area',
+		array(
+			'labels'                => array(
+				'name'                     => _x( 'WordPress Area', 'post type general name' ),
+				'singular_name'            => _x( 'WordPress Area', 'post type singular name' ),
+				'menu_name'                => _x( 'WordPress Areas', 'admin menu' ),
+				'name_admin_bar'           => _x( 'WordPress Area', 'add new on admin bar' ),
+				'add_new'                  => _x( 'Add New', 'Block' ),
+				'add_new_item'             => __( 'Add New WordPress Area' ),
+				'new_item'                 => __( 'New WordPress Area' ),
+				'edit_item'                => __( 'Edit WordPress Area' ),
+				'view_item'                => __( 'View WordPress Area' ),
+				'all_items'                => __( 'All WordPress Areas' ),
+				'search_items'             => __( 'Search WordPress Areas' ),
+				'not_found'                => __( 'No WordPress area found.' ),
+				'not_found_in_trash'       => __( 'No WordPress areas found in Trash.' ),
+				'filter_items_list'        => __( 'Filter WordPress areas list' ),
+				'items_list_navigation'    => __( 'WordPress areas list navigation' ),
+				'items_list'               => __( 'WordPress areas list' ),
+				'item_published'           => __( 'WordPress area published.' ),
+				'item_published_privately' => __( 'WordPress area published privately.' ),
+				'item_reverted_to_draft'   => __( 'WordPress area reverted to draft.' ),
+				'item_scheduled'           => __( 'WordPress area scheduled.' ),
+				'item_updated'             => __( 'WordPress area updated.' ),
+			),
+			'public'                => false,
+			'show_ui'               => true,
+			'show_in_menu'          => false,
+			'show_in_rest'          => true,
+			'capabilities'          => array(
+				// You need to be able to edit posts, in order to read blocks in their raw form.
+				'read'                   => 'edit_posts',
+				// You need to be able to publish posts, in order to create blocks.
+				'create_posts'           => 'edit_theme_options',
+				'edit_posts'             => 'edit_theme_options',
+				'edit_published_posts'   => 'edit_theme_options',
+				'delete_published_posts' => 'edit_theme_options',
+				'edit_others_posts'      => 'edit_theme_options',
+				'delete_others_posts'    => 'edit_theme_options',
+			),
+			'map_meta_cap'          => true,
+			'supports'              => array(
+				'title',
+				'editor',
+			),
+		)
+	);
+}
+add_action( 'init', 'create_wp_area_post_type' );
+
+
